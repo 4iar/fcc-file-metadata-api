eca2098a427a3dc4ddaeb7a55cf9d1a8e312aa42
4iar <4iar@users.noreply.github.com>
2016-07-28 19:22:10 +0100

    Add upload page

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 // let Heroku set the port
 const port = process.env.PORT || 5000;
 
+app.use(express.static('public'));
+
 app.get('/', (request, response) => {
   response.json({hello: "world"})
 });
